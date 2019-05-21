8175c80256fae7a11429aed42e99957c1d237d2a
soumendhar1310 <soumendhar1310@gmail.com>
2019-05-21 12:55:40 +0800

    Second try

--- src/main/java/com/javacodegeeks/example/HelloWorldController.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello Java Code Geeks! Taking Changes in place.";
+		return "Hello Java Code Geeks! Taking Changes in place. Testing.";
 	}
 }
