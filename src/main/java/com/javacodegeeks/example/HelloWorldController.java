a0ffcf6b9539fa7bbde891a1646bee80768699c2
soumendhar1310 <soumendhar1310@gmail.com>
2020-05-15 15:18:21 +0800

    UPdating for Jenkins test

--- src/main/java/com/javacodegeeks/example/HelloWorldController.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello Java Code Geeks! Taking Changes in place. Testing.";
+		return "Hello Java Code Geeks! Taking Changes in place. Testing continued......";
 	}
 }
