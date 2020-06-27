113cf84507ad4728ed86039e7874c7660cd5ab45
soumendhar1310 <soumendhar1310@gmail.com>
2020-06-27 23:10:06 +0800

    27 june 3

--- src/main/java/com/javacodegeeks/example/HelloWorldController.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello Java Code Geeks! Taking Changes in place. Testing continued one after another......on 27th June 2";
+		return "Hello Java Code Geeks! Taking Changes in place. Testing continued one after another......on 27th June 3";
 	}
 }
